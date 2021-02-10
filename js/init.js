6fb71d9c450cf2ccc70cca8629a40d00a90d7052
Prabhjot Mattu <theprabh@gmail.com>
2021-02-10 16:19:14 -0600

    Added to experience section

--- js/init.js
+++ js/init.js
@@ -6,3 +6,18 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+ $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+
